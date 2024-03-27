eb159e633abb63fc3a8e0fa47d32e09f62aabae0
Nima Saadati <nimodbdev@gmail.com>
2024-03-27 16:26:48 +0400

    update static file and fix url logo image

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -27,6 +27,6 @@ urlpatterns = (
         path("dashboard/", include("members.urls")),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 )
 # static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
